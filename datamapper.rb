939a0f5bc1d05694df2b98c488603263043fbdb5
mkristian <m.kristian@web.de>
2009-03-22 02:12:31 +0800

    use datamapper4rail to get things working
    Signed-off-by: Jeremy McAnally <jeremymcanally@gmail.com>

--- datamapper.rb
+++ datamapper.rb
@@ -1,28 +1,32 @@
 # inspired by http://www.rowtheboat.com/archives/32
 
-# have specs
-plugin 'rspec',
-  :git => 'git://github.com/dchelimsky/rspec.git'
-plugin 'rspec-rails',
-  :git => 'git://github.com/dchelimsky/rspec-rails.git'
-
-generate("rspec")
 
 # get all datamapper related gems (assume sqlite3 to be database)
 gem "addressable", :lib => "addressable/uri"
-gem "data_objects"
 gem "do_sqlite3"
-gem "dm-core"
-gem "rails_datamapper"
+gem 'dm-validations'
+gem 'dm-timestamps'
+gem "rspec", :lib => false
+gem "rspec-rails", :lib => false
+gem "datamapper4rail", :lib => 'datamapper4rails' # excuse the typo
 
 rake "gems:install"
 
-# install datamapper rake tasks
-generate("dm-install")
+# have specs
+generate("rspec")
 
-# fix config/environment.rb to work with datamapper instead of active_record
-run "sed -i config/environment.rb -e 's/#.*config.plugins.*/config.plugins = [ :rails_datamapper, :all ]/'"
+# install datamapper rake tasks
+generate("dm_install")
+
+# fix config files to work with datamapper instead of active_record
 run "sed -i config/environment.rb -e 's/#.*config.frameworks.*/config.frameworks -= [ :active_record ]/'"
+run "sed -i spec/spec_helper.rb -e 's/^\\s*config[.]/#\\0/'"
+run "sed -i test/test_helper.rb -e 's/^[^#]*fixtures/#\\0/'"
+
+# fix a problem with missing class constants for models woth relations
+initializer 'preload_models.rb', <<-CODE
+require 'datamapper4rails/preload_models'
+CODE
 
 # set up git
 git :init
